a9c4eddfffd196a566f22ab080ee77ba038579ea
J0oan <jmitjansblasco@gmail.com>
2017-02-04 12:33:45 +0100

    Upgrade write method of Mobile communicator (now 2 packages are send without ACK)

--- src/Communicators/MSCommunicator.py
+++ src/Communicators/MSCommunicator.py
@@ -29,14 +29,14 @@ class RFMCommunicator(CommunicatorBase.CommunicatorBase):
         # def __init__(self, freqBand, nodeID, networkID, isRFM69HW = False, intPin = 18, rstPin = 29, spiBus = 0, spiDevice = 0):
         self.trans = RFM69.RFM69(RF69_433MHZ, 2, 2, True, 18, 29, 0, 0)
         # Set max power 20 dBm
-        self.trans.setPowerLevel(RF_PALEVEL_OUTPUTPOWER_11111)
+        # self.trans.setPowerLevel(RF_PALEVEL_OUTPUTPOWER_11111) #done by default when initializing
         #self.trans.setHighPower(True)  #done by default when initializing
         #self.trans.setHighPowerRegs(False) #done by default when changeing to rx mode
         self.trans.rcCalibration()
 
 
     def read(self):
-        self.trans.receiveBegin()
+        self.trans.receiveBegin() #Initialize parameters and set rx mode
         while not self.trans.receiveDone():
             time.sleep(.05)
         if self.trans.ACKRequested():
@@ -44,9 +44,14 @@ class RFMCommunicator(CommunicatorBase.CommunicatorBase):
 
 
     def write(self, data):
-        #altitude, pressure, temperature, acceleration, velocity, latitude, longitude
-        toSend = pack('d', data.current_data.time, data.current_data.altitude, data.current_data.pressure, data.current_data.temperature, data.current_data.acceleration, data.current_data.velocity, data.current_data.latitude, data.current_data.longitude )
-        self.trans.send(1,toSend)
+        #data to send: time, altitude, pressure, temperature, roll, pitch, yaw.
+        #dimension of toSend = 61 to take advantage of the built in AES/CRC we want to limit the frame size to the internal FIFO size (66 bytes - 3 bytes overhead)
+        toSend = pack('d', data.current_data.time, data.current_data.altitude, data.current_data.pressure, data.current_data.temperature, data.current_data.orientation.get('roll'), data.current_data.orientation.get('pitch'), data.current_data.orientation.get('yaw'))
+        self.trans.send(1,toSend) #When finish sending changes to rx mode
+
+        #tdata to send: time, acceleration, velocity, latitude, longitude
+        toSend = pack('d', data.current_data.time, data.current_data.acceleration, data.current_data.velocity, data.current_data.latitude, data.current_data.longitude)
+        self.trans.send(1,toSend) #When finish sending changes to rx mode
 
     def shutDown(self):
         self.trans.shutdown()
